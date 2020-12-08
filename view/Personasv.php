c1ef76ead88f73b4c2194d43a7373900e29a7961
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2020-12-07 21:34:40 -0500

    Se adicionan las 3 operaciones de base de datos pendientes

--- view/Personasv.php
+++ view/Personasv.php
@@ -7,14 +7,25 @@
 </head>
 <body>
 
-<h1>Lista de personas registradas</h1>
+<h1>Entidad personas</h1>
 
+<h2>Registro de personas</h2>
+<form name="form1" method="POST" action="index.php?accion=guardar">
+
+<p>Nombre: <input type="text" name="txtnombre" id="txtnombre"></p>
+<p>Edad: <input type="text" name="txtedad" id="txtedad"></p>
+<p><input type="submit" name="btnguardar" value="Guardar"></p>
+</form>
+
+<br>
 <table>
     <thead> <!-- Encabezado de la tabla -->
         <tr> <!-- Fila -->
             <th>Id</th> <!-- Columnas-->
             <th>Nombre</th> <!-- Columnas-->
             <th>Edad</th> <!-- Columnas-->
+            <th>Modificar</th>
+            <th>Eliminar</th>
         </tr>
     </thead>
     <tbody>
@@ -25,6 +36,8 @@
             <td><?php echo $dato['id']; ?></td>
             <td><?php echo $dato['nombre']; ?></td>
             <td><?php echo $dato['edad']; ?></td>
+            <td><a href="index.php?accion=modificar&id=<?php echo $dato['id']; ?>">Modificar</a></td>
+            <td><a href="index.php?accion=eliminar&id=<?php echo $dato['id']; ?>">Eliminar</a></td>
         </tr>
         
     <?php endforeach; ?>
